66d8dc7c6cac5c9807a812f52603869d90086357
Jing Kang <ngjingkang97@gmail.com>
2020-05-06 01:21:33 +0800

    solved hooks

--- frontend/src/Pages/Staff/components/Promotions.js
+++ frontend/src/Pages/Staff/components/Promotions.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from "react";
-
+import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import { Grid } from "@material-ui/core";
 import NavBar from "../../../components/Navigation/Navigation";
@@ -20,6 +20,114 @@ import {
   TableContainer,
   Paper,
 } from "@material-ui/core";
+import MaterialTable, { Column } from "material-table";
+
+//const [columnState, setColumnState] = useState("");
+
+// function MaterialTableDemo() {
+const MaterialTableDemo = (props) => {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: "Name", field: "name" },
+      { title: "Surname", field: "surname" },
+      { title: "Birth Year", field: "birthYear", type: "numeric" },
+      {
+        title: "Birth Place",
+        field: "birthCity",
+        lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+      },
+    ],
+    data: [
+      { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
+      {
+        name: "Zerya Betül",
+        surname: "Baran",
+        birthYear: 2017,
+        birthCity: 34,
+      },
+    ],
+  });
+  return (
+    <div>
+      {/* <NavBar history={this.props.history} /> */}
+      <Grid container spacing={6}>
+        <Grid item lg={6} sm={6} xl={6} xs={12}>
+          <Sidebar pageWrapId={"page-wrap"} outerContainerId={"Home"} />
+        </Grid>
+        <Grid container item spacing={4} id="page-wrap">
+          <Grid item lg={6} sm={6} xl={6} xs={12}></Grid>
+
+          <Grid item lg={12} sm={12} xl={12} xs={12}>
+            <Button
+              color="primary"
+              size="small"
+              variant="contained"
+              //   onClick={addRow}
+            >
+              Add Promotion
+            </Button>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Food Item</TableCell>
+                    <TableCell>Duration</TableCell>
+                    <TableCell>Discount</TableCell>
+                    <TableCell>Free Delivery</TableCell>
+                    <TableCell>Condition</TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+            </TableContainer>
+          </Grid>
+        </Grid>
+      </Grid>
+      <MaterialTable
+        title="Editable Example"
+        columns={state.columns}
+        data={state.data}
+        editable={{
+          onRowAdd: (newData) =>
+            new Promise((resolve) => {
+              setTimeout(() => {
+                resolve();
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data.push(newData);
+                  return { ...prevState, data };
+                });
+              }, 600);
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve) => {
+              setTimeout(() => {
+                resolve();
+                if (oldData) {
+                  setState((prevState) => {
+                    const data = [...prevState.data];
+                    data[data.indexOf(oldData)] = newData;
+                    return { ...prevState, data };
+                  });
+                }
+              }, 600);
+            }),
+          onRowDelete: (oldData) =>
+            new Promise((resolve) => {
+              setTimeout(() => {
+                resolve();
+                setState((prevState) => {
+                  const data = [...prevState.data];
+                  data.splice(data.indexOf(oldData), 1);
+                  return { ...prevState, data };
+                });
+              }, 600);
+            }),
+        }}
+      />
+      //{" "}
+    </div>
+  );
+};
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -41,72 +149,23 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// const AddPromo = (props) => {
-//   const { className, ...rest } = props;
-
-//   const classes = useStyles();
-
-//   const [selectedCategory, setSelectedCategory] = useState("restaurant");
-//   const [RestaurantReview, setRestaurantReview] = useState("");
-//   const [RReview, setRReview] = useState("");
-//   const [RRating, setRRating] = useState("");
-
-//   const [DeliveryReview, setDeliveryReview] = useState("");
-//   const [DRating, setDRating] = useState("");
-
-//   const handleSelectChange = (e) => {
-//     setSelectedCategory(e.target.value);
-//   };
-//   const handleRestaurantReview = (e) => {
-//     setRestaurantReview(e.target.value);
-//   };
-//   const handleRReview = (e) => {
-//     setRReview(e.target.value);
-//   };
-//   const handleRRating = (e) => {
-//     setRRating(e.target.value);
-//   };
-//   const handleDeliveryReview = (e) => {
-//     setDeliveryReview(e.target.value);
-//   };
-
-//   const handleDRating = (e) => {
-//     setDRating(e.target.value);
-//   };
-
-//   // QUERY: INSERT data
-//   const handleEnterButton = () => {
-//     switch (selectedCategory) {
-//       case "restaurant":
-//         console.log(RRating);
-//         console.log(RReview);
-
-//         break;
-//       case "delivery":
-//         console.log(DRating);
-
-//         break;
-//       default:
-//         console.log("error");
-//     }
-//     props.onClick();
-//   };
-// };
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
 class Promotions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
   render() {
     return (
       <div>
+        <p>You clicked {this.state.count} times</p>
+        {/* <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button> */}
+
         <NavBar history={this.props.history} />
+        <MaterialTableDemo></MaterialTableDemo>
         <Grid container spacing={6}>
           <Grid item lg={6} sm={6} xl={6} xs={12}>
             <Sidebar pageWrapId={"page-wrap"} outerContainerId={"Home"} />
@@ -115,25 +174,11 @@ class Promotions extends Component {
             <Grid item lg={6} sm={6} xl={6} xs={12}></Grid>
 
             <Grid item lg={12} sm={12} xl={12} xs={12}>
-              {/* <AccountDetails
-                firstname={this.state.firstname}
-                lastname={this.state.lastname}
-                email={this.state.email}
-                password={this.state.password}
-                id={this.state.id}
-                resid={this.state.resid}
-                history={this.props.history} */}
-              {/* <RestaurantDetails
-                resid={this.state.resid}
-                resname={this.state.resname}
-                min={this.state.min}
-                history={this.props.history}
-              /> */}
               <Button
                 color="primary"
                 size="small"
                 variant="contained"
-                onClick={addRow}
+                onClick={() => this.setState({ count: this.state.count + 1 })}
               >
                 Add Promotion
               </Button>
@@ -157,8 +202,9 @@ class Promotions extends Component {
     );
   }
 }
-const addRow = () => {
-  //   props.onClick();
+MaterialTableDemo.propTypes = {
+  className: PropTypes.string,
 };
 
+//export default MaterialTableDemo;
 export default Promotions;
