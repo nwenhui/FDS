a326139c83933b3197c34c994bd12c028fb397d4
limsiying <siying1899@gmail.com>
2020-05-07 02:24:08 +0800

    seed data

--- database/init.sql
+++ database/init.sql
@@ -32,11 +32,45 @@ CREATE TABLE FoodItem (
     PRIMARY KEY (ItemID)
 );
 
-INSERT INTO FoodItem VALUES (DEFAULT, 'Spaghetti', 5, 100);
+--seed for Western Stall, 1-6
+INSERT INTO FoodItem VALUES (DEFAULT, 'Spaghetti', 5, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Carbonara', 5, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Mushroom Soup', 3, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Seafood Chowder', 3, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Chicken Teriyaki Pizza', 13, 90);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Garlic Bread', 1, 300);
+
+--seed for drinks, 7-12
 INSERT INTO FoodItem VALUES (DEFAULT, 'Coke', 2, 130);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Sprite', 2, 100);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Milkshake', 3, 100);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Tea', 1, 300);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Coffee', 1, 300);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Milo Dinosaur', 4, 100);
+
+--seed for chinese, 13-18
 INSERT INTO FoodItem VALUES (DEFAULT, 'Rice', 1, 100);
-INSERT INTO FoodItem VALUES (DEFAULT, 'Nasi Briyani', 5, 70);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Stir Fry', 5, 70);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Orange Chicken', 5, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Sliced Fish Soup', 7, 100);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Kway Teow', 5, 70);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Kang Kong', 3, 200);
+
+--seed for malay, 19-24
+INSERT INTO FoodItem VALUES (DEFAULT, 'Nasi Briyani', 5, 230);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Mee Goreng', 5, 100);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Nasi Lemak', 5, 100);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Satay', 13, 170);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Mee Siam', 5, 100);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Ayam Penyet Set', 9, 200);
+
+--seed for indian, 25-30
 INSERT INTO FoodItem VALUES (DEFAULT, 'Prata', 8, 200); 
+INSERT INTO FoodItem VALUES (DEFAULT, 'Butter Chicken Curry', 5, 450);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Naan', 3, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Cheese Prata', 13, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Egg Prata', 13, 200);
+INSERT INTO FoodItem VALUES (DEFAULT, 'Muruku', 3, 500);
 
 CREATE TABLE Restaurant (
     ResID SERIAL,
@@ -56,18 +90,59 @@ CREATE TABLE Listings (
     ResID INTEGER,
     ItemID INTEGER,
     PRIMARY KEY (ResID, ItemID),
-    FOREIGN KEY (ResID) REFERENCES Restaurant 
-        on update cascade 
-        on delete cascade,
-    FOREIGN KEY (ItemID) REFERENCES FoodItem
-        on update cascade
+    FOREIGN KEY (ResID) REFERENCES Restaurant on delete cascade,
+    FOREIGN KEY (ItemID) REFERENCES FoodItem on delete cascade
 );
+
+--seed for western
+INSERT INTO Listings VALUES (1, 1);
+INSERT INTO Listings VALUES (1, 2);
+INSERT INTO Listings VALUES (1, 3);
+INSERT INTO Listings VALUES (1, 4);
+INSERT INTO Listings VALUES (1, 5);
+INSERT INTO Listings VALUES (1, 6);
+
+--seed for drinks
+INSERT INTO Listings VALUES (2, 7);
+INSERT INTO Listings VALUES (2, 8);
+INSERT INTO Listings VALUES (2, 9);
+INSERT INTO Listings VALUES (2, 10);
+INSERT INTO Listings VALUES (2, 11);
+INSERT INTO Listings VALUES (2, 12);
+
+--seed for chinese
+INSERT INTO Listings VALUES (3, 13);
+INSERT INTO Listings VALUES (3, 14);
+INSERT INTO Listings VALUES (3, 15);
+INSERT INTO Listings VALUES (3, 16);
+INSERT INTO Listings VALUES (3, 17);
+INSERT INTO Listings VALUES (3, 18);
+
+--seed for malay
+INSERT INTO Listings VALUES (4, 19);
+INSERT INTO Listings VALUES (4, 20);
+INSERT INTO Listings VALUES (4, 21);
+INSERT INTO Listings VALUES (4, 22);
+INSERT INTO Listings VALUES (4, 23);
+INSERT INTO Listings VALUES (4, 24);
+
+--seed for indian
+INSERT INTO Listings VALUES (5, 25);
+INSERT INTO Listings VALUES (5, 26);
+INSERT INTO Listings VALUES (5, 27);
+INSERT INTO Listings VALUES (5, 28);
+INSERT INTO Listings VALUES (5, 29);
+INSERT INTO Listings VALUES (5, 30);
 
 CREATE TABLE Category (
     catid serial,
     CategoryName VARCHAR(20),
     PRIMARY KEY (catid)
 );
+
+INSERT INTO Category VALUES (DEFAULT, 'For Sharing');
+INSERT INTO Category VALUES (DEFAULT, 'Main');
+INSERT INTO Category VALUES (DEFAULT, 'Finger Food');
 
 CREATE TABLE Classifies (
     catid integer,
@@ -76,6 +151,26 @@ CREATE TABLE Classifies (
     FOREIGN KEY (catid) REFERENCES Category,
     FOREIGN KEY (ItemID) REFERENCES FoodItem
 );
+
+INSERT INTO Classifies VALUES (1, 6);
+INSERT INTO Classifies VALUES (1, 15);
+INSERT INTO Classifies VALUES (1, 18);
+INSERT INTO Classifies VALUES (1, 22);
+INSERT INTO Classifies VALUES (1, 26);
+INSERT INTO Classifies VALUES (2, 1);
+INSERT INTO Classifies VALUES (2, 2);
+INSERT INTO Classifies VALUES (2, 14);
+INSERT INTO Classifies VALUES (2, 16);
+INSERT INTO Classifies VALUES (2, 19);
+INSERT INTO Classifies VALUES (2, 20);
+INSERT INTO Classifies VALUES (2, 21);
+INSERT INTO Classifies VALUES (2, 23);
+INSERT INTO Classifies VALUES (2, 24);
+INSERT INTO Classifies VALUES (3, 25);
+INSERT INTO Classifies VALUES (3, 27);
+INSERT INTO Classifies VALUES (3, 28);
+INSERT INTO Classifies VALUES (3, 29);
+INSERT INTO Classifies VALUES (3, 30);
 
 CREATE TABLE Inventory (
     itemid integer,
@@ -110,6 +205,31 @@ INSERT INTO Inventory VALUES (2, 123, DEFAULT);
 INSERT INTO Inventory VALUES (3, 90, DEFAULT);
 INSERT INTO Inventory VALUES (4, 33, DEFAULT);
 INSERT INTO Inventory VALUES (5, 197, DEFAULT);
+INSERT INTO Inventory VALUES (6, 0, DEFAULT);
+INSERT INTO Inventory VALUES (7, 13, DEFAULT);
+INSERT INTO Inventory VALUES (8, 9, DEFAULT);
+INSERT INTO Inventory VALUES (9, 33, DEFAULT);
+INSERT INTO Inventory VALUES (10, 37, DEFAULT);
+INSERT INTO Inventory VALUES (11, 0, DEFAULT);
+INSERT INTO Inventory VALUES (12, 13, DEFAULT);
+INSERT INTO Inventory VALUES (13, 9, DEFAULT);
+INSERT INTO Inventory VALUES (14, 33, DEFAULT);
+INSERT INTO Inventory VALUES (15, 37, DEFAULT);
+INSERT INTO Inventory VALUES (16, 0, DEFAULT);
+INSERT INTO Inventory VALUES (17, 13, DEFAULT);
+INSERT INTO Inventory VALUES (18, 9, DEFAULT);
+INSERT INTO Inventory VALUES (19, 33, DEFAULT);
+INSERT INTO Inventory VALUES (20, 37, DEFAULT);
+INSERT INTO Inventory VALUES (21, 0, DEFAULT);
+INSERT INTO Inventory VALUES (22, 13, DEFAULT);
+INSERT INTO Inventory VALUES (23, 9, DEFAULT);
+INSERT INTO Inventory VALUES (24, 33, DEFAULT);
+INSERT INTO Inventory VALUES (25, 37, DEFAULT);
+INSERT INTO Inventory VALUES (26, 0, DEFAULT);
+INSERT INTO Inventory VALUES (27, 13, DEFAULT);
+INSERT INTO Inventory VALUES (28, 9, DEFAULT);
+INSERT INTO Inventory VALUES (29, 33, DEFAULT);
+INSERT INTO Inventory VALUES (30, 37, DEFAULT);
 
 CREATE TABLE Staff (
     Id SERIAL,
@@ -122,6 +242,30 @@ CREATE TABLE Staff (
     FOREIGN KEY (RestaurantID) REFERENCES Restaurant on delete cascade
 );
 
+INSERT INTO Staff VALUES (DEFAULT, 1, 'seed1_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 1, 'seed2_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 1, 'seed3_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 1, 'seed4_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 1, 'seed5_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 2, 'seed6_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 2, 'seed7_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 2, 'seed8_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 2, 'seed9_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 3, 'seed10_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 3, 'seed11_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 3, 'seed12_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 3, 'seed13_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 4, 'seed14_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 4, 'seed15_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 4, 'seed16_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 4, 'seed17_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 4, 'seed18_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 5, 'seed19_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 5, 'seed20_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 5, 'seed21_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 5, 'seed22_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Staff VALUES (DEFAULT, 5, 'seed23_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+
 CREATE TABLE Manager (
     Id SERIAL,
     email VARCHAR(100) unique NOT NULL,
@@ -130,6 +274,13 @@ CREATE TABLE Manager (
     password VARCHAR(100) NOT NULL,
     PRIMARY KEY (Id)
 );
+
+INSERT INTO Manager VALUES (DEFAULT, 'seed24_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Manager VALUES (DEFAULT, 'seed25_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Manager VALUES (DEFAULT, 'seed26_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Manager VALUES (DEFAULT, 'seed27_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Manager VALUES (DEFAULT, 'seed28_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Manager VALUES (DEFAULT, 'seed29_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
 
 CREATE TABLE Promotion (
     PromotionID SERIAL,
@@ -164,9 +315,10 @@ CREATE TRIGGER promotion_date_trigger
 
 INSERT INTO Promotion VALUES (DEFAULT, DEFAULT, DEFAULT, DEFAULT, 
     DEFAULT, now(), '2020-09-28 01:00:00');
-
-<<<<<<< HEAD
-=======
+INSERT INTO Promotion VALUES (DEFAULT, DEFAULT, DEFAULT, DEFAULT, 
+    DEFAULT, now(), '2020-05-18 01:00:00');
+    INSERT INTO Promotion VALUES (DEFAULT, DEFAULT, DEFAULT, DEFAULT, 
+    DEFAULT, now(), '2021-05-08 01:00:00');
 
 
 create table restaurantpromotion (
@@ -175,14 +327,17 @@ create table restaurantpromotion (
     primary key (promotionid, resid)
 );
 
+INSERT INTO restaurantpromotion VALUES (1, 2);
+INSERT INTO restaurantpromotion VALUES (2, 4);
+
 create table fdspromotion (
     promotionid integer unique references promotion,
     managerid integer references Manager,
     primary key (promotionid, managerid)
 );
 
+INSERT INTO fdspromotion VALUES (3, 3);
 
->>>>>>> 447edac4808f430ee5c12548f013421213a440b9
 CREATE TABLE Rider (
     Id SERIAL,
     email varchar(100) unique not null,
@@ -192,11 +347,19 @@ CREATE TABLE Rider (
     PRIMARY KEY (Id)
 );
 
+INSERT INTO Rider VALUES (DEFAULT, 'seed30_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Rider VALUES (DEFAULT, 'seed31_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Rider VALUES (DEFAULT, 'seed32_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+INSERT INTO Rider VALUES (DEFAULT, 'seed33_email@gmail.com', 'seed_first_name', 'seed_second_name', 'thisisapassword');
+
 CREATE TABLE PartTime (
     Id INTEGER,
     PRIMARY KEY (Id),
     FOREIGN KEY (Id) REFERENCES Rider on delete cascade
 );
+
+INSERT INTO PartTime VALUES (1);
+INSERT INTO PartTime VALUES (2);
 
 CREATE TABLE FullTime (
     Id INTEGER,
@@ -204,12 +367,14 @@ CREATE TABLE FullTime (
     FOREIGN KEY (Id) REFERENCES Rider on delete cascade
 );
 
+INSERT INTO FullTime VALUES (3);
+INSERT INTO FullTime VALUES (4);
+
 CREATE TABLE Shifts (
     Id INTEGER,
-    ShiftID SERIAL,
-    StartTime TIMESTAMP NOT NULL,
-    EndTime TIMESTAMP NOT NULL,
-    PRIMARY KEY (ShiftID, Id),
+    StartTime TIMESTAMP,
+    EndTime TIMESTAMP,
+    PRIMARY KEY (Id, StartTime, EndTime),
     FOREIGN KEY (Id) REFERENCES Rider ON DELETE CASCADE
 );
 
@@ -272,6 +437,7 @@ CREATE TABLE Delivers (
 
 CREATE TABLE Receipt (
     orderid integer,
+    promotionid INTEGER,
     GainedPoints INTEGER, 
     UsedPoints INTEGER,
     DeliveryFee INTEGER,
@@ -304,5 +470,5 @@ CREATE TABLE Salary (
     Comission INTEGER,
     BaseSalary INTEGER,
     PRIMARY KEY (Id),
-    FOREIGN KEY (Id) REFERENCES Rider --ON DELETE CASCADE
+    FOREIGN KEY (Id) REFERENCES Rider ON DELETE CASCADE
 );
